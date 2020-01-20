e0ac6e83d21cd5d790e1e7799799441ac36fcd75
Cyrille BERTOLA <cbertola@student.42.fr>
2020-01-20 23:50:52 +0100

    renvoi

--- Cub3D/parsing.c
+++ Cub3D/parsing.c
@@ -20,13 +20,12 @@ int		check_map(t_map *map)
 
 	i = 0;
 	j = 0;
-		printf("%s\n",map->tab_map[j]);
 	while (map->tab_map[j][i])
 	{
 		if (map->tab_map[j][i++] != '1')
 			return (ft_error(ERROR_MAP_F_WALL));
 	}
-	map->line_len = i;
+	map->line_len = --i;
 	while (++j < map->tab_len)
 	{
 		if (map->tab_map[j][0] != '1')
@@ -44,7 +43,6 @@ int		check_map(t_map *map)
 
 int		check_elem(char *str, t_elem *elem, t_map *map)
 {
-	char *strmap;
 
 	if (elem->bit_elem < 255)
 	{
@@ -53,11 +51,10 @@ int		check_elem(char *str, t_elem *elem, t_map *map)
 	}
 	if (*str == '1')
 	{
-		strmap = ft_strjoin(map->str_map, str);
-		map->str_map = ft_strsubstr(strmap, " ");
+		map->str_map = ft_strjoin_free(map->str_map, str, 1);
+		map->str_map = ft_strsubstr_free(map->str_map, " ");
 		if (map->line_len == 0)
 			map->line_len = ft_strlen(map->str_map);
-		free(strmap);
 		map->tab_len++;
 		return (1);
 	}
@@ -74,7 +71,6 @@ int        main(int argc, char **argv)
 	t_map		*map;
 
 	(void)argc;
-	printf("%s\n",ft_strsubstr("t es test tes ", " "));
 	elem = malloc(sizeof(t_elem));
 	map = malloc(sizeof(t_map));
 	elem->check = (char **)malloc(sizeof(char *) * 8);
@@ -92,6 +88,10 @@ int        main(int argc, char **argv)
 	{
 		return (-1);
 	}
-	map->tab_map = ft_split(map->str_map, '\n');
+	if (ft_how_many(map->str_map, "NSEW") > 1)
+			return (-1);
+	if (ft_how_many(map->str_map, "NSEW") < 1)
+			return (-1);
+	map->tab_map = ft_splitnum(map->str_map, map->line_len);
 	return (check_map(map));
 }
