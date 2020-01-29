4071b8f1cce10bcbf38d3e8325c4ad4247e4593e
Cyrille BERTOLA <cbertola@student.42.fr>
2020-01-29 20:42:42 +0100

    20.01.29 20:42:42 affichage map optimise

--- Cub3D/display_map.c
+++ Cub3D/display_map.c
@@ -6,33 +6,47 @@
 /*   By: cbertola <cbertola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 01:59:15 by cbertola          #+#    #+#             */
-/*   Updated: 2020/01/29 19:11:05 by cbertola         ###   ########.fr       */
+/*   Updated: 2020/01/29 20:42:01 by cbertola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void		color_square(int i, t_mini *mini, int color, int mult)
+{
+	int x;
+	int y;
+
+	x = -1;
+	while (++x < mini->t_case - 1)
+	{
+		y = -1;
+		while (++y < mini->t_case - 1)
+			mini->img->buffer[i + y + (x * mini->t_case * mult)] = color;
+	}
+}
+
 void		map_color_case(t_data *data, int x, int y, int t_case)
 {
-	t_elem *elem;
-	int i;
+	t_elem	*elem;
+	int		i;
 
 	i = y + (x * (data->elem->map->y_max * t_case));
 	elem = data->elem;
 	if (y % t_case == 0 || x % t_case == 0)
-			data->mini->img->buffer[i] = (int)0x000000;
+		data->mini->img->buffer[i] = (int)0x000000;
 	else
 	{
 		if (elem->map->tab_map[x / t_case][y / t_case] == '1' ||
 				(y / t_case) >= ft_strlen(elem->map->tab_map[x / t_case]))
-			data->mini->img->buffer[i] = (int)0xCBC9C8;
+			color_square(i, data->mini, (int)0xCBC9C8, elem->map->y_max);
 		else if (elem->map->tab_map[x / t_case][(y / t_case)] == 'N' ||
 				elem->map->tab_map[x / t_case][y / t_case] == 'E' ||
 				elem->map->tab_map[x / t_case][y / t_case] == 'S' ||
 				elem->map->tab_map[x / t_case][y / t_case] == 'W')
-			data->mini->img->buffer[i] = (int)0xFF0000;
+			color_square(i, data->mini, (int)0xFF0000, elem->map->y_max);
 		else
-			data->mini->img->buffer[i] = (int)0xFFFFFF;
+			color_square(i, data->mini, (int)0xFFFFFF, elem->map->y_max);
 	}
 }
 
@@ -40,28 +54,26 @@ void		display_map(t_data *data, t_elem *elem, int t_case)
 {
 	int		x;
 	int		y;
+	t_image	*img;
 
 	x = t_case * elem->map->x_max;
 	y = t_case * elem->map->y_max;
-	data->mini->img->image = mlx_new_image(data->mlx_ptr, y, x);
-	data->mini->img->buffer = (int *)mlx_get_data_addr(data->mini->img->image,
-			&data->mini->img->bpp, &data->mini->img->size_l,
-			&data->mini->img->endian);
-	x = 0;
+	img = data->mini->img;
+	img->image = mlx_new_image(data->mlx_ptr, y, x);
+	img->buffer = (int *)mlx_get_data_addr(img->image, &img->bpp,
+			&img->size_l, &img->endian);
+	x = 1;
 	while (x <= (t_case * elem->map->x_max))
 	{
-		y = 0;
+		y = 1;
 		while (y <= (t_case * elem->map->y_max))
 		{
 			map_color_case(data, x, y, t_case);
-			y++;
+			y += t_case;
 		}
-		if ( x == (t_case * elem->map->y_max) - 10000)
-			while(1)
-			{}
-		x++;
+		x += t_case;
 	}
-	mlx_put_image_to_window(data->mlx_ptr, data->mlx_win, data->mini->img->image, 0, 0); 
+	mlx_put_image_to_window(data->mlx_ptr, data->mlx_win, img->image, 0, 0);
 }
 
 void		mini_map(t_data *data, t_elem *elem)
@@ -79,6 +91,4 @@ void		mini_map(t_data *data, t_elem *elem)
 		printf("Map too big to be displayed %d\n", mini->t_case);
 	else
 		display_map(data, elem, mini->t_case);
-
-
 }
