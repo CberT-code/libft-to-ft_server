1fbb5801c6a637071d318f79df2b7a83caee12ec
Cyrille BERTOLA <cbertola@student.42.fr>
2020-02-19 01:30:56 +0100

    20.02.19 01:30:56

--- Cub3D/parsing/parsing_elem.c
+++ Cub3D/parsing/parsing_elem.c
@@ -6,7 +6,7 @@
 /*   By: cbertola <cbertola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/19 21:43:13 by cbertola          #+#    #+#             */
-/*   Updated: 2020/02/19 01:21:20 by cbertola         ###   ########.fr       */
+/*   Updated: 2020/02/19 01:30:20 by cbertola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,10 +98,10 @@ void		parsing_elem(char *str, t_elem *elem)
 		fill_str(3, str + 3, elem);
 	if (ft_start_str("S ", str) > 0)
 		fill_str(4, str + 2, elem);
-	if (ft_start_str("f ", str) > 0)
+	if (ft_start_str("F ", str) > 0)
 		fill_int_rgb(5, str + 2, elem->f, &(elem->bit_elem));
-	if (ft_start_str("c ", str) > 0)
+	if (ft_start_str("C ", str) > 0)
 		fill_int_rgb(6, str + 2, elem->c, &(elem->bit_elem));
-	if (ft_start_str("r ", str) > 0)
+	if (ft_start_str("R ", str) > 0)
 		fill_int(7, str + 2, elem->r, &(elem->bit_elem));
 }
